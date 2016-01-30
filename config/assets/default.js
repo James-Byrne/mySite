a2c1a10bd4c4908fe77bb91b3ee14cd86f91bdaa
James Byrne <jamesdesbyrne@gmail.com>
2016-01-30 15:08:08 +0000

    removed uneeded requirements

--- config/assets/default.js
+++ config/assets/default.js
@@ -4,8 +4,6 @@ module.exports = {
   client: {
     lib: {
       css: [
-        // 'public/lib/bootstrap/dist/css/bootstrap.css',
-        // 'public/lib/bootstrap/dist/css/bootstrap-theme.css',
         'public/lib/angular-material/angular-material.css'
       ],
       js: [
