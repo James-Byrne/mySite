5501b4a6de2db929f5244e9f4715aa083d17f544
James Byrne <jamesdesbyrne@gmail.com>
2016-02-05 18:03:31 +0000

    Fixes the background-image displaying on smaller devices and cleanup of a controller

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -2,15 +2,9 @@
 
 angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
   function ($scope, $state, Authentication, Menus) {
-    // Expose view variables
-    $scope.$state = $state;
-    console.log($state.$current.$self);
-    console.log('the current state name : ' + $state.current.$name);
 
     // if we are on the home screen hide the logo in the top left
-    if ($state.current.name === 'homePageDev'){
-      $scope.hideLogo = true;
-    }
+    $scope.hideLogo = true;
 
     $scope.authentication = Authentication;
 
