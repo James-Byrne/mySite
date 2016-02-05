f4a71a63f6f96e126b01fa3a1e4624230fa476ce
James Byrne <jamesdesbyrne@gmail.com>
2016-02-05 20:39:57 +0000

    Changed the production port

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,25 +1,11 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
+angular.module('core')
+.controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
   function ($scope, $state, Authentication, Menus) {
-
     // if we are on the home screen hide the logo in the top left
     $scope.hideLogo = true;
 
     $scope.authentication = Authentication;
-
-    // Get the topbar menu
-    $scope.menu = Menus.getMenu('topbar');
-
-    // Toggle the menu items
-    $scope.isCollapsed = false;
-    $scope.toggleCollapsibleMenu = function () {
-      $scope.isCollapsed = !$scope.isCollapsed;
-    };
-
-    // Collapsing the menu after navigation
-    $scope.$on('$stateChangeSuccess', function () {
-      $scope.isCollapsed = false;
-    });
   }
 ]);
